ab68dc26d61efdfd2bcb04af34692561c0c7dfff
raptor2101 <raptor2101@gmx.de>
2013-12-01 22:36:48 +0100

    ADD: MessageListing CHG: adopt changes from the referenced libraries

--- src/de/raptor2101/BattleWorldsKronos/Connector/ConnectorApp.java
+++ src/de/raptor2101/BattleWorldsKronos/Connector/ConnectorApp.java
@@ -1,14 +1,28 @@
 package de.raptor2101.BattleWorldsKronos.Connector;
 
+import android.app.Activity;
 import de.raptor2101.BattleWorldsKronos.Connector.AbstractConnectorApp;
 import de.raptor2101.BattleWorldsKronos.Connector.NotificationService;
 import de.raptor2101.BattleWorldsKronos.Connector.Gui.Activities.GameListingActivity;
+import de.raptor2101.BattleWorldsKronos.Connector.Gui.Activities.MessageListingActivity;
 
 public class ConnectorApp extends AbstractConnectorApp{
   
     @Override
     public void onCreate() {
-      NotificationService.setResponseActivities(GameListingActivity.class, null);
+      NotificationService.setResponseActivities(GameListingActivity.class, MessageListingActivity.class);
       super.onCreate();
+    }
+
+    @Override
+    public Class<? extends Activity> getGameListingActivityClass() {
+      // TODO Auto-generated method stub
+      return GameListingActivity.class;
+    }
+
+    @Override
+    public Class<? extends Activity> getMessageListingActivityClass() {
+      // TODO Auto-generated method stub
+      return MessageListingActivity.class;
     }
 }
